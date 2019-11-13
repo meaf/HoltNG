51bbad6c2991a31ddd5193825892351575e7058a
meaffs <vakhovitch@gmail.com>
2019-11-13 22:34:56 +0200

    google map accuracy circles

--- src/main/java/com/meaf/apeps/view/components/GoogleMapPreset.java
+++ src/main/java/com/meaf/apeps/view/components/GoogleMapPreset.java
@@ -1,9 +1,15 @@
 package com.meaf.apeps.view.components;
 
 import com.vaadin.tapio.googlemaps.GoogleMap;
+import com.vaadin.tapio.googlemaps.client.LatLon;
 import com.vaadin.tapio.googlemaps.client.overlays.GoogleMapMarker;
+import com.vaadin.tapio.googlemaps.client.overlays.GoogleMapPolygon;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
 
 public class GoogleMapPreset extends GoogleMap {
   private GoogleMapPreset(String apiKey, String clientId, String language) {
@@ -21,6 +27,37 @@ public class GoogleMapPreset extends GoogleMap {
 
     setSizeFull();
     setMinZoom(3);
-    setMaxZoom(10);
+    setMaxZoom(13);
+  }
+
+  @Override
+  public void addMarker(GoogleMapMarker marker) {
+    addAccuracyRing(marker);
+    super.addMarker(marker);
+  }
+
+  private void addAccuracyRing(GoogleMapMarker marker) {
+    GoogleMapPolygon polygon = new GoogleMapPolygon();
+    polygon.setCoordinates(createCircle(marker.getPosition()));
+    polygon.setFillOpacity(0.3);
+    polygon.setFillColor("red");
+    polygon.setStrokeOpacity(0);
+    polygon.setGeodesic(false);
+    addPolygonOverlay(polygon);
+  }
+
+  private List<LatLon> createCircle(LatLon position) {
+    List<LatLon> pts = new ArrayList<>();
+    int ptsNumber = 360;
+
+    double rad = 0.01;
+
+    for(int i=0; i<ptsNumber; i++){
+      double theta = i*(2*Math.PI/ptsNumber);
+      double x = position.getLat() + rad*sin(theta);
+      double y = position.getLon() + rad*cos(theta);
+      pts.add(new LatLon(x, y));
+    }
+    return pts;
   }
 }
